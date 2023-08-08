e4456783ce320f468b6bde59694626a82a65f13c
Andi Syafrianda <andisyafrianda@gmail.com>
2023-08-08 12:09:34 +0700

    split navbar

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,20 +1,26 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import AppLayout from '@/Layouts/AppLayout';
 import { Head } from '@inertiajs/react';
+import Container from '@/Components/Container';
 
 export default function Dashboard({ auth }) {
     return (
-        <AuthenticatedLayout
-            user={auth.user}
-            header={<h2 className='font-semibold text-xl text-gray-800 leading-tight'>Dashboard</h2>}>
+        <>
             <Head title='Dashboard' />
 
             <div className='py-12'>
-                <div className='max-w-7xl mx-auto sm:px-6 lg:px-8'>
-                    <div className='bg-white overflow-hidden shadow-sm sm:rounded-lg'>
+                <Container>
+                    <div className='overflow-hidden bg-white shadow-sm sm:rounded-lg'>
                         <div className='p-6 text-gray-900'>You're logged in!</div>
                     </div>
-                </div>
+                </Container>
             </div>
-        </AuthenticatedLayout>
+        </>
     );
 }
+
+Dashboard.layout = (page) => (
+    <AppLayout
+        header={<h2 className='text-xl font-semibold leading-tight text-gray-800'>Dashboard</h2>}
+        children={page}
+    />
+);
